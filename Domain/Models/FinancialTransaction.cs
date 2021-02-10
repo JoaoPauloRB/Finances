fe4b316f962098ca052659d34398320fa082a436
João Paulo Rodrigues Barbosa <joaopaulo.r.b@outlook.com>
2021-02-10 17:08:51 -0300

    Update

--- Domain/Models/FinancialTransaction.cs
+++ Domain/Models/FinancialTransaction.cs
@@ -8,6 +8,8 @@ namespace Domain.Models
     public int FinancialTransactionId { get; set; }
     public string Description { get; set; }
     public float Amount { get; set; }
+    public int AccountId { get; set; }
+    public int CategotyId { get; set; }
     public FinancialTransactionType Type { get; set; }
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public DateTime Creation { get; set; }
